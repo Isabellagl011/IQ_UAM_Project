bc6f4c27c0bc7e0070325159f595b5c3f5cacea2
Isabella <isabella.grajalesl@autonoma.edu.co>
2023-03-31 17:20:32 -0500

    Route connection

--- src/models/imageModel.js
+++ src/models/imageModel.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const imageSchema = mongoose.Schema({
-    id: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    name:{
-        type:String,
-        require:true
-    }
-
-
+  id: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  answer: {
+    type: this.schema.ObjetId,
+    ref: 'imageModel',
+  },
 });
-module.exports= mongoose.model('ImageCollection',imageSchema)
+module.exports = mongoose.model('ImageCollection', imageSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const personRouter = require('./people.router');
-
+const imageRouter = require('./image.router');
 function routerApi(app) {
   const router = express.Router();
   /*Endpoint estático http://localhost:5000/api/v1 */
